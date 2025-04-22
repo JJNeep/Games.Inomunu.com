575cf4dc8ae80a0749e79df814598e1442cad8f8
JJNeep <grosvenor.streaming@gmail.com>
2025-04-22 09:43:40 +0800

    Update automatic_download.js

--- javascript/automatic_download.js
+++ javascript/automatic_download.js
@@ -1,6 +1,7 @@
 const productElement = document.getElementById('download_file');
 let file = parseString(productElement.dataset.file);
 
+document.getElementById('');
 window.onload = function() {
   document.getElementById('my_iframe').src = file;
 };
