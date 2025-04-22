a51338877614a766df6c86622f1e86656aabcc94
JJNeep <grosvenor.streaming@gmail.com>
2025-04-22 11:23:12 +0900

    Delete javascript directory

--- javascript/automatic_download.js
+++
@@ -1,7 +0,0 @@
-const productElement = document.getElementById('download_file');
-let file = parseString(productElement.dataset.file);
-
-document.getElementById('debug_text') = file
-window.onload = function() {
-  document.getElementById('my_iframe').src = file;
-};
